cf9d0c79b9373ff2c184a69e84f74f3839a650bf
elinarnten <elin.arnten@hotmail.com>
2022-02-14 13:58:13 +0100

    added drag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="container">
       <motion.Button
+        drag
+        dragConstraints={{
+          top: -50,
+          left: -50,
+          right: 50,
+          bottom: 50,
+        }}
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         className="click-button"
