9494507c6af0c9d5c629bb88438d6a5ceb842adf
elinarnten <elin.arnten@hotmail.com>
2022-02-15 08:55:50 +0100

    created own component for Button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,10 @@
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 import Button from "./Button";
 
 function App() {
   return (
     <div className="container">
-      <motion.Button
-        drag
-        dragConstraints={{
-          top: -50,
-          left: -50,
-          right: 50,
-          bottom: 50,
-        }}
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className="click-button"
-        onClick={() => null}
-      >
-        Click Me!
-      </motion.Button>
+      <Button />
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,22 @@
 //import "./index.css";
+import { motion } from "framer-motion";
 function Button(props) {
-  return <button onClick={props.onClick}>Click me!</button>;
+  return (
+    <motion.Button
+      drag
+      dragConstraints={{
+        top: -50,
+        left: -50,
+        right: 50,
+        bottom: 50,
+      }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      className="click-button"
+      onClick={() => null}
+    >
+      Click Me!
+    </motion.Button>
+  );
 }
 export default Button;
